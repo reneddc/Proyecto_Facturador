7f7aa48f182c3abb3a3e06d8e25093c6c566bed5
Rene <ddcrene@gmail.com>
2022-12-02 16:38:27 -0400

    Tests ver 2

--- FACTURADOR_API/Tests/BillerTests.cs
+++ FACTURADOR_API/Tests/BillerTests.cs
@@ -53,25 +53,25 @@ namespace Tests
                 Biller expectedBiller = new Biller();
                 Biller currentBiller = new Biller();
 
-                var CI = "3654781";
-                string startDate = "01/12/2022 0:00:00";
-                string finishDate = "31/12/2022 23:59:59";
+                var CI = "1457890";
+                string startDate = "01/06/2022 0:00:00";
+                string finishDate = "30/06/2022 23:59:59";
 
                 currentBiller = _billerService.getBiller(CI, DateTime.Parse(startDate), DateTime.Parse(finishDate));
 
                 expectedBiller.UserCI = CI;
-                expectedBiller.NameUser = "Persona 2";
+                expectedBiller.NameUser = "Persona 1";
                 expectedBiller.TypePlan = "Pre Pago";
-                expectedBiller.Category = "Pre Pago Familia";
-                expectedBiller.AmountTotalSMS = 1;
-                expectedBiller.TotalSMSPrice = 0.2;
-                expectedBiller.AmountTotalHoursNormal = 2;
-                expectedBiller.TotalNormalPrice = 3.4;
+                expectedBiller.Category = "Pre Pago Libre";
+                expectedBiller.AmountTotalSMS = 2;
+                expectedBiller.TotalSMSPrice = 0.4;
+                expectedBiller.AmountTotalHoursNormal = 3;
+                expectedBiller.TotalNormalPrice = 5.1;
                 expectedBiller.AmountTotalHoursReducido = 0;
                 expectedBiller.TotalReducidoPrice = 0;
-                expectedBiller.AmountTotalHoursSuperReducido = 4;
-                expectedBiller.TotalSuperReducidoPrice = 2;
-                expectedBiller.TotalPayment = 5.6;
+                expectedBiller.AmountTotalHoursSuperReducido = 1;
+                expectedBiller.TotalSuperReducidoPrice = 0.5;
+                expectedBiller.TotalPayment = 6;
 
                 Assert.AreEqual(expectedBiller, currentBiller);
             }
