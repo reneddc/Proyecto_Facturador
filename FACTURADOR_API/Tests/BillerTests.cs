91d5b93615de5861654fff2ad2cea97d9062bed7
Rene <ddcrene@gmail.com>
2022-12-02 16:48:35 -0400

    Tests ver 3

--- FACTURADOR_API/Tests/BillerTests.cs
+++ FACTURADOR_API/Tests/BillerTests.cs
@@ -13,68 +13,66 @@ namespace Tests
             _billerService = billerService;
         }
 
+        
         [TestMethod]
-        public void TestMethod1()
+        void shouldReturnTheBillerPerson2()
         {
-            [TestMethod]
-            void shouldReturnTheBillerPerson2()
-            {
 
-                Biller expectedBiller = new Biller();
-                Biller currentBiller = new Biller();
+            Biller expectedBiller = new Biller();
+            Biller currentBiller = new Biller();
 
-                var CI = "3654781";
-                string startDate = "01/12/2022 0:00:00";
-                string finishDate = "31/12/2022 23:59:59";
+            var CI = "3654781";
+            string startDate = "01/12/2022 0:00:00";
+            string finishDate = "31/12/2022 23:59:59";
 
-                currentBiller = _billerService.getBiller(CI, DateTime.Parse(startDate), DateTime.Parse(finishDate));
+            currentBiller = _billerService.getBiller(CI, DateTime.Parse(startDate), DateTime.Parse(finishDate));
 
-                expectedBiller.UserCI = CI;
-                expectedBiller.NameUser = "Persona 2";
-                expectedBiller.TypePlan = "Pre Pago";
-                expectedBiller.Category = "Pre Pago Familia";
-                expectedBiller.AmountTotalSMS = 1;
-                expectedBiller.TotalSMSPrice = 0.2;
-                expectedBiller.AmountTotalHoursNormal = 2;
-                expectedBiller.TotalNormalPrice = 3.4;
-                expectedBiller.AmountTotalHoursReducido = 0;
-                expectedBiller.TotalReducidoPrice = 0;
-                expectedBiller.AmountTotalHoursSuperReducido = 4;
-                expectedBiller.TotalSuperReducidoPrice = 2;
-                expectedBiller.TotalPayment = 5.6;
+            expectedBiller.UserCI = CI;
+            expectedBiller.NameUser = "Persona 2";
+            expectedBiller.TypePlan = "Pre Pago";
+            expectedBiller.Category = "Pre Pago Familia";
+            expectedBiller.AmountTotalSMS = 1;
+            expectedBiller.TotalSMSPrice = 0.2;
+            expectedBiller.AmountTotalHoursNormal = 2;
+            expectedBiller.TotalNormalPrice = 3.4;
+            expectedBiller.AmountTotalHoursReducido = 0;
+            expectedBiller.TotalReducidoPrice = 0;
+            expectedBiller.AmountTotalHoursSuperReducido = 4;
+            expectedBiller.TotalSuperReducidoPrice = 2;
+            expectedBiller.TotalPayment = 5.6;
 
-                Assert.AreEqual(expectedBiller, currentBiller);
-            }
+            Assert.AreEqual(expectedBiller, currentBiller);
+        }
 
-            [TestMethod]
-            void shouldReturnTheBillerPerson1()
-            {
+        [TestMethod]
+        void shouldReturnTheBillerPerson1()
+        {
 
-                Biller expectedBiller = new Biller();
-                Biller currentBiller = new Biller();
+            Biller expectedBiller = new Biller();
+            Biller currentBiller = new Biller();
 
-                var CI = "1457890";
-                string startDate = "01/06/2022 0:00:00";
-                string finishDate = "30/06/2022 23:59:59";
+            var CI = "1457890";
+            string startDate = "01/06/2022 0:00:00";
+            string finishDate = "30/06/2022 23:59:59";
 
-                currentBiller = _billerService.getBiller(CI, DateTime.Parse(startDate), DateTime.Parse(finishDate));
+            currentBiller = _billerService.getBiller(CI, DateTime.Parse(startDate), DateTime.Parse(finishDate));
 
-                expectedBiller.UserCI = CI;
-                expectedBiller.NameUser = "Persona 1";
-                expectedBiller.TypePlan = "Pre Pago";
-                expectedBiller.Category = "Pre Pago Libre";
-                expectedBiller.AmountTotalSMS = 2;
-                expectedBiller.TotalSMSPrice = 0.4;
-                expectedBiller.AmountTotalHoursNormal = 3;
-                expectedBiller.TotalNormalPrice = 5.1;
-                expectedBiller.AmountTotalHoursReducido = 0;
-                expectedBiller.TotalReducidoPrice = 0;
-                expectedBiller.AmountTotalHoursSuperReducido = 1;
-                expectedBiller.TotalSuperReducidoPrice = 0.5;
-                expectedBiller.TotalPayment = 6;
+            expectedBiller.UserCI = CI;
+            expectedBiller.NameUser = "Persona 1";
+            expectedBiller.TypePlan = "Pre Pago";
+            expectedBiller.Category = "Pre Pago Libre";
+            expectedBiller.AmountTotalSMS = 2;
+            expectedBiller.TotalSMSPrice = 0.4;
+            expectedBiller.AmountTotalHoursNormal = 3;
+            expectedBiller.TotalNormalPrice = 5.1;
+            expectedBiller.AmountTotalHoursReducido = 0;
+            expectedBiller.TotalReducidoPrice = 0;
+            expectedBiller.AmountTotalHoursSuperReducido = 1;
+            expectedBiller.TotalSuperReducidoPrice = 0.5;
+            expectedBiller.TotalPayment = 6;
 
-                Assert.AreEqual(expectedBiller, currentBiller);
-            }
+            Assert.AreEqual(expectedBiller, currentBiller);
         }
     }
+    
 }
